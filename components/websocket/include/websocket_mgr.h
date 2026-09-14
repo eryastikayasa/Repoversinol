6c67c47c25fe1157c4f014a8981f96350158407b
eryastikayasa <eryastikayasa@gmail.com>
2026-09-14 21:40:44 +0800

    Expose current audio queue depth metrics

--- components/websocket/include/websocket_mgr.h
+++ components/websocket/include/websocket_mgr.h
@@ -14,6 +14,8 @@ void websocket_app_start(void);
 bool websocket_is_connected(void);
 bool websocket_healthcheck(void);
 uint32_t websocket_get_reconnect_count(void);
+UBaseType_t websocket_get_tx_queue_depth(void);
+UBaseType_t websocket_get_rx_queue_depth(void);
 void websocket_disconnect(void);
 bool websocket_tx_enqueue_audio(const uint8_t *data, size_t len, uint32_t generation);
 extern uint32_t websocket_connection_generation;
