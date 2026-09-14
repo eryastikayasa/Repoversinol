6f30b5e35e3c0a85ea2b4f51591f4a135232892d
eryastikayasa <eryastikayasa@gmail.com>
2026-09-14 21:37:53 +0800

    Expose Wi-Fi reconnect metric

--- components/wifi_manager/include/wifi_manager.h
+++ components/wifi_manager/include/wifi_manager.h
@@ -6,14 +6,7 @@
 #define WIFI_SSID "Qrick WiFi"
 #define WIFI_PASS "1komalima"
 
-/*
- * ============================================================
- * WIFI MANAGER - V7.0.4
- * ============================================================
- */
-
 void wifi_init_sta(void);
-
 bool wifi_wait_for_connection(uint32_t timeout_ms);
-
 bool wifi_is_ready(void);
+uint32_t wifi_get_reconnect_count(void);
